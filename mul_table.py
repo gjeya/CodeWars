7d2765474073c0d0f6cf38fa32f46e2e3402e31c
gjeya <107497010+gjeya@users.noreply.github.com>
2025-02-15 12:09:39 +0000

    Add files via upload

---
+++ mul_table.py
@@ -0,0 +1,11 @@
+x = 2
+n = 7
+
+output_list = []
+for num_till_where_I_want in range (1,n+1):
+    mul = x * num_till_where_I_want
+    #print (a)
+    output_list.append(mul)
+    # print(a)
+print (output_list)
+
